fd6c51ed66404cf164b8b4a58a10b571474016e9
Munezero Christian <chamsf5@aol.com>
2022-08-18 16:02:38 +0200

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, uniqueness: {case_sensitive: false}, length: {minimum:6}
+    validates :email, presence: true, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
 end
