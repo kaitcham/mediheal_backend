15d0de3dfb030dc1dd9f66c18274b3248a520697
Munezero Christian <chamsf5@aol.com>
2022-08-18 16:07:43 +0200

    Generate a token  when a user sign up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :require_login, only: [:create, :index]
+
+    def index
+        users = User.all
+        render json: users, status: :ok
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: {user: UserSerializer.new(user), jwt: token}, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 end
